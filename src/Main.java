d8aa294f62a90e9cbb272e604104f87e281d9a7c
duongt <trinhtungduong2019@gmail.com>
2023-02-28 11:13:17 +0700

    th1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Hello world!");
+//        Date date = new Date ();
+//        System.out.println("Time now: "+date);
+        int i = 10;
+        float f = 20.5f;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i = "+i);
+        System.out.println("f = "+f);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println();
+    }
+}
